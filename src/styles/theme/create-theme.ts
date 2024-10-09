baade2f0f9005990fd83e1759f6c47fe7374a5f4
Hoenfvr <swaghnn86@gmail.com>
2024-10-09 21:14:16 +0700

    start v2

---
+++ src/styles/theme/create-theme.ts
@@ -0,0 +1,56 @@
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
+
+import { colorSchemes } from './color-schemes';
+import { components } from './components/components';
+import { shadows } from './shadows';
+import type { Theme } from './types';
+import { typography } from './typography';
+
+declare module '@mui/material/styles/createPalette' {
+  interface PaletteRange {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+    950: string;
+  }
+
+  interface Palette {
+    neutral: PaletteRange;
+  }
+
+  interface PaletteOptions {
+    neutral?: PaletteRange;
+  }
+
+  interface TypeBackground {
+    level1: string;
+    level2: string;
+    level3: string;
+  }
+}
+
+export function createTheme(): Theme {
+  const theme = extendTheme({
+    breakpoints: { values: { xs: 0, sm: 600, md: 900, lg: 1200, xl: 1440 } },
+    components,
+    colorSchemes,
+    shadows,
+    shape: { borderRadius: 8 },
+    typography,
+    // palette: {
+    //   background: {
+    //     default: '#fff', // เปลี่ยนสีพื้นหลังของเพจ
+    //     paper: '#fff' // เปลี่ยนสีพื้นหลังขององค์ประกอบต่าง ๆ เช่น การ์ด
+    //   },
+    // },
+  });
+
+  return theme;
+}
